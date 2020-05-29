211581be7b2903e1501297cf33ea155a6cd26319
seo ji-hwan <seojihwan213@naver.com>
2020-05-29 18:20:49 +0900

    add controller

---
+++ controller/videoController.js
@@ -0,0 +1,5 @@
+export const video = (req, res) => res.send("video");
+export const upload = (req, res) => res.send("upload");
+export const video_detail = (req, res) => res.send("video_detail");
+export const edit_video = (req, res) => res.send("edit_video");
+export const delete_video = (req, res) => res.send("delete_video");
