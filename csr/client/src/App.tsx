8adb546062e489c8ce4bbd60e00df77ac96c3834
seojihwan <seojihwan213@naver.com>
2020-11-09 22:52:02 +0900

    ♻️ change: (Home, Upload) component to render

--- csr/client/src/App.tsx
+++ csr/client/src/App.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { NavBar } from './component';
 import { Home, Login, SignUp, Upload, VideoDetail } from './pages';
-
+import { v4 } from 'uuid';
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <NavBar />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" render={() => <Home key={v4()} />} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/video/upload" component={Upload} />
+          <Route
+            exact
+            path="/video/upload"
+            render={() => <Upload key={v4()} />}
+          />
           <Route exact path="/video/:id" component={VideoDetail} />
         </Switch>
       </BrowserRouter>
