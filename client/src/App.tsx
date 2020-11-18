260e871d537bd6857d4958f4bfbd636324a3751b
seojihwan <seojihwan213@naver.com>
2020-11-18 23:28:24 +0900

    ♻️ Refactor: use constants for route path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,28 +4,27 @@ import { NavBar } from './component';
 import { Home, Login, SignUp, Upload, VideoDetail } from './pages';
 import { v4 } from 'uuid';
 import { Results, User } from './pages';
+import * as ROUTES from './constants/routes';
 import './App.css';
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" render={() => <Home key={v4()} />} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route
-            exact
-            path="/video/upload"
-            render={() => <Upload key={v4()} />}
-          />
-          <Route exact path="/video/:id" component={VideoDetail} />
-          <Route exact path="/results" component={Results} />
-          <Route exact path="/user/:id" component={User} />
-        </Switch>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path={ROUTES.HOME} render={() => <Home key={v4()} />} />
+        <Route exact path={ROUTES.LOGIN} component={Login} />
+        <Route exact path={ROUTES.SIGNUP} component={SignUp} />
+        <Route
+          exact
+          path={ROUTES.UPLOAD}
+          render={() => <Upload key={v4()} />}
+        />
+        <Route exact path={ROUTES.VIDEO} component={VideoDetail} />
+        <Route exact path={ROUTES.RESULTS} component={Results} />
+        <Route exact path={ROUTES.USER} component={User} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ client/src/constants/routes.ts
@@ -0,0 +1,7 @@
+export const HOME = '/';
+export const LOGIN = '/login';
+export const SIGNUP = '/signup';
+export const UPLOAD = '/video/upload';
+export const VIDEO = '/video/:id';
+export const RESULTS = '/results';
+export const USER = '/user/:id';
